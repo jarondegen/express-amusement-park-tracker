15ca5f1764365958aeba899a9de341e8d3d5544e
Jaron Degen <jaron.degen90@gmail.com>
2020-08-29 00:23:21 +0200

    stages 5-7 complete

--- routes.js
+++ routes.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const environment = require('./config/index')
-
+const db = require('./db/models/index.js')
 
 router.get('/', (req, res) => {
     res.render('index', { title: 'Home' });
 });
 
-// if(environment !== 'production'){
-//     router.get('/error-test', () => {
-//         throw new Error('This is a test error.');
-//     });
-// }
+const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
+
+
+router.get('/parks', asyncHandler(async (req, res, next) => {
+    const parks = await db.Park.findAll({
+        order: [['parkName', 'ASC']],
+    })
+    res.render('park-list', { title: 'Parks', parks});
+}));
+
 
 module.exports = router;
